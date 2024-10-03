8b3033379572a62386b0ed1e37801cc699a043f6
Motch <motchHmmF@gmail.com>
2024-10-03 14:45:47 +1000

    Misc mob spawning changes

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 RPG using rozukke/mcpp
 
 Currently Non-Functional
+
+WARNINGS!
+This program Changes the following gamerules to the following values:
+    `doWeatherCycle = false`
+    `doDaylightCycle = false`
+    `doMobLoot = false`
+    `doMobSpawning = false`
+    `doFireTick = false`
--- v1/rpg.cpp
+++ v1/rpg.cpp
@@ -5,40 +5,51 @@ class RPG {
     private:
         mcpp::MinecraftConnection mc;
         mcpp::Coordinate origin;
-        mcpp::Coordinate printer;
+        mcpp::Coordinate pos;
 
-        double difficultyMultiplier;
-        double shopPrices;
-        int startingEquipment;
+        
 
     public:
         RPG();
-        void SelectSettings();
         void TestArea();
+        void SetUp();
+        void SpawnWave();
 };
 
 int main() {
     RPG rpg;
-    rpg.SelectSettings();
     rpg.TestArea();
+    rpg.SetUp();
+    rpg.SpawnWave();
 }
 
 RPG::RPG() {
     origin = mc.getPlayerTilePosition();
 }
 
-void RPG::SelectSettings() {
-    // Create a thing that builds the settings area
-    difficultyMultiplier = 1;
-    shopPrices = 1.0;
-    startingEquipment = 1;
-}
-
 void RPG::TestArea() {
     Structure test;
     test.TESTA();
-    test.Print(origin, mc);
-    // test.SummonSpawners(origin,mc);
-    // std::cout<<blocks.max_size();
+    test.Print(origin, mc);    
+}
+
+void RPG::SetUp() {
+    // mc.doCommand("gamerule sendCommandFeedback false");
+    mc.doCommand("gamerule doWeatherCycle false");
+    mc.doCommand("gamerule doDaylightCycle false");
+    mc.doCommand("gamerule doMobLoot false");
+    mc.doCommand("gamerule doMobSpawning false");
+    // mc.doCommand("gamemode adventure @a");
+    mc.doCommand("gamerule doFireTick false");
+    mc.doCommand("time set night");
+    mc.doCommand("effect give @a minecraft:night_vision infinite 1 true");
+    mc.doCommand("effect give @a minecraft:saturation 10 10 true");
+    mc.doCommand("effect give @a minecraft:regeneration 10 10 true");
+    // mc.doCommand("gamerule sendCommandFeedback true");
     
+    pos = origin;
+    pos.x += 10;
+    pos.y += 5;
+    pos.z += 10;
+    mc.setPlayerPosition(pos);
 }
